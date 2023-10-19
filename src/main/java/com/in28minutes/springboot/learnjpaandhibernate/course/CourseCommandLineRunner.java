6d3fbcac0a054c896d7bb31977ceb462a2178002
insh007 <aliinshad007@gmail.com>
2023-10-19 13:34:54 +0530

    getting started with Spring Data JPA

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -4,28 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner{
 	
+//	@Autowired
+//	private CourseJpaRepository repository;
+	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
 	
 	@Override
 	public void run(String... args) {
-		repository.insert(new Course(1, "Learn JPA", "Coursera"));
-		repository.insert(new Course(2, "Learn C++", "Coursera"));
-		repository.insert(new Course(3, "Learn Java", "Coursera"));
+		repository.save(new Course(1, "Learn Spring Data JPA", "Coursera"));
+		repository.save(new Course(2, "Learn C++", "Coursera"));
+		repository.save(new Course(3, "Learn Java", "Coursera"));
 		
-		repository.deleteById(1);
+		repository.deleteById(2l);
 		
-		System.out.println(repository.findById(2));
+		System.out.println(repository.findById(1l));
 		
 	}
 	
 }
 
-/* 
- * To run the JdbcTemplate we need to implement CommandLineRunner .
- */
+/* Notes:
+ *  1. To run the JdbcTemplate we need to implement CommandLineRunner .
+ *  2. Now save, deleteById, findById is provided by using Spring Data JPA for that need to 
+ *  create an interface and then need to implements an interface which is "JpaRepository"
+ */ 
