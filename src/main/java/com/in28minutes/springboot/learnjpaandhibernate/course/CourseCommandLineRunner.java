9e7c2c24518008242e48f2125fbfb52d3248abc2
insh007 <aliinshad007@gmail.com>
2023-10-19 12:23:47 +0530

    use of JPA and EnitityManager

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -1,26 +1,26 @@
-package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
+package com.in28minutes.springboot.learnjpaandhibernate.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
 
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner{
+public class CourseCommandLineRunner implements CommandLineRunner{
 	
 	@Autowired
-	private CourseJdbcRepository repository;
+	private CourseJpaRepository repository;
 	
 	@Override
 	public void run(String... args) {
-		repository.insert(new Course(1, "Learn AWS", "Coursera"));
+		repository.insert(new Course(1, "Learn JPA", "Coursera"));
 		repository.insert(new Course(2, "Learn C++", "Coursera"));
 		repository.insert(new Course(3, "Learn Java", "Coursera"));
 		
-		repository.deleteByID(1);
+		repository.deleteById(1);
 		
-		System.out.println(repository.selectByID(2));
+		System.out.println(repository.findById(2));
 		
 	}
 	
